2a1e5a2958fcbfc0dfdf072fcae6ec36adb8c5e9
Joseeeeph <josephbaria89@gmail.com>
2026-02-09 11:55:01 +0800

    pvc type options

--- app/api/send-schedule-update-email/route.ts
+++ app/api/send-schedule-update-email/route.ts
@@ -52,7 +52,7 @@ export async function POST(req: Request) {
       await transporter.sendMail({
         from: `"${process.env.SMTP_FROM_NAME}" <${process.env.SMTP_USER}>`,
         to: trainee.email,
-       cc: "sales@petrosphere.com.ph, training@petrosphere.com.ph",
+       //cc: "sales@petrosphere.com.ph, training@petrosphere.com.ph",
        // cc: "jlb@petrosphere.com.ph",
         subject: `📅 Updated Training Schedule – ${courseName}`,
         html: htmlMessage,
